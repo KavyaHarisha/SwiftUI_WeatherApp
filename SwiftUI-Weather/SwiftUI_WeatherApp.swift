74f74aec7cb2781d44e6e715d46f47d1f5943496
kathmuri <kathmuri@mobiquityinc.com>
2021-10-16 18:12:50 +0530

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Kavya Athmuri on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
